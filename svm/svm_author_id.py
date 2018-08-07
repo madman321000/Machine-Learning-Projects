5a046a275292fbca78e8ecaabe1cc9a63d565bf9
madman321000 <maneesh.madala@gmail.com>
2018-08-07 18:29:35 -0400

    Used an SVM to calculate the author of emails

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,12 +19,16 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=10000)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
 
-
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+num = [c for c in pred if c == 1]
+print acc
+print len(num)
